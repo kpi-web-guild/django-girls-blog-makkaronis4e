c31668dc1f457fa533d5a1efc8085b0c3de20a4a
makkaronis4e <makkaronis4e@gmail.com>
2017-12-28 20:13:11 +0200

    Link model with template

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 """Views for blog app."""
-from django.shortcuts import render  # noqa: F401
+from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 
 def post_list(request):
     """Render page with list of posts."""
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
